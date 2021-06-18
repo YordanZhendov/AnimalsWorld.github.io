96951abbf60f868a8bcc90a6a4d305723317f645
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-06-18 11:15:19 +0300

    asdf

--- script.js
+++ script.js
@@ -1,34 +1,110 @@
 window.onload = function () {
+    weatheInfo();
+    getCoordintes();
+    window.setInterval(function () { weatheInfo() }, 1000);
     focusIndexPageReg();
     createMenu();
     window.onscroll = function () { myFunction() };
     clicked();
 
 }
-function focusIndexPageReg(){
-    document.getElementById('inputEmail42').focus({preventScroll:false});
+function focusIndexPageReg() {
+    document.getElementById('inputEmail42').focus({ preventScroll: false });
 }
-function focusIndexPageSign(){
-    document.getElementById('inputEmail45').focus({preventScroll:false});
+function focusIndexPageSign() {
+    document.getElementById('inputEmail45').focus({ preventScroll: false });
 }
 //LOGIN REGISTER FORM
-var register=document.getElementsByClassName('register')[0];
-var sign=document.getElementsByClassName('sign')[0];
+var register = document.getElementsByClassName('register')[0];
+var sign = document.getElementsByClassName('sign')[0];
 
-sign.style.marginTop=(969/window.innerHeight*6);
-register.style.marginTop=(969/window.innerHeight*6);
+sign.style.marginTop = (969 / window.innerHeight * 6);
+register.style.marginTop = (969 / window.innerHeight * 6);
 
-function signingIN(){
-    register.style.display='none';
-    sign.style.display='block';
+function signingIN() {
+    register.style.display = 'none';
+    sign.style.display = 'block';
     focusIndexPageReg();
-    
+
 };
-function registerIn(){
-    sign.style.display='none';
-    register.style.display='block';
+function registerIn() {
+    sign.style.display = 'none';
+    register.style.display = 'block';
     focusIndexPageSign()
 };
+
+// weather button
+
+function showWeather() {
+    document.getElementById('weatherWindow').style.display = 'block';
+};
+function hideWeather() {
+    document.getElementById('weatherWindow').style.display = 'none';
+};
+//date
+function weatheInfo() {
+    var todayt = new Date();
+    var seconds = todayt.getSeconds();
+    var minutes = todayt.getMinutes();
+    var hours = todayt.getHours();
+    if (seconds < 10) {
+        document.getElementById('currentTime').innerText = hours + ':' + minutes + ':0' + seconds;
+    } else {
+        document.getElementById('currentTime').innerText = hours + ':' + minutes + ':' + seconds;
+    }
+};
+//city
+// Step 1: Get user coordinates
+function getCoordintes() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+  
+    function success(pos) {
+        var crd = pos.coords;
+        var lat = crd.latitude.toString();
+        var lng = crd.longitude.toString();
+        var coordinates = [lat, lng];
+        console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+        getCity(coordinates);
+        return;
+  
+    }
+  
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+  
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+  
+// Step 2: Get city name
+function getCity(coordinates) {
+    var xhr = new XMLHttpRequest();
+    var lat = coordinates[0];
+    var lng = coordinates[1];
+  
+    // Paste your LocationIQ token below.
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=YOUR_PRIVATE_TOKEN&lat=" +
+    lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
+    xhr.addEventListener("readystatechange", processRequest, false);
+  
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            var city = response.address.city;
+            document.getElementById('currentCity').innerText=city;
+            return;
+        }
+    }
+}
+  
+
+// navigation menu
 
 function createMenu() {
 
@@ -96,7 +172,7 @@ function myFunction(parami) {
     if (parami == undefined) {
         ulTags[index].style.borderLeft = '5px solid lightblue';
         ulTags[index].style.fontWeight = '700';
-        
+
     } else {
         ulTags[parami].style.borderLeft = '5px solid lightblue';
         ulTags[parami].style.fontWeight = '700';
